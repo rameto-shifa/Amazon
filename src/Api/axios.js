71d0f9bd3901a987e8c8ea6a5d9c021d992454cb
rahmet <155022635+rameto-shifa@users.noreply.github.com>
2024-05-28 22:11:52 +0300

    all frontend

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
-    // baseURL: "http://127.0.0.1:4002/clone-3b183/us-central1/api",
-    // baseURL: "https://amazon-api-deploy-1-qtc6.onrender.com"
-  });
-  
-  export { axiosInstance };
+    // baseURL: "https://amazon-api-deploy-1-qtc6.onrender.com",
+    // baseURL: "http://localhost:5001",
+// deployed version of amazon backend
+    baseURL: "https://amazon-api-deploy-5o05.onrender.com/"
+    });
+    
+    export { axiosInstance };
