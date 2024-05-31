38eebe4b91939ae64d15687b826b5d4609fc0325
rahmet <155022635+rameto-shifa@users.noreply.github.com>
2024-05-31 19:30:35 +0300

    backend link on axios

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
+    baseURL: "https://amazon-api-deploy-2-iwsg.onrender.com",
     // baseURL: "https://amazon-api-deploy-1-qtc6.onrender.com",
-    // baseURL: "http://localhost:5001",
-    baseURL: "https://amazon-api-deploy-1-71kp.onrender.com",
+    // baseURL: "https://amazon-api-deploy-1-71kp.onrender.com",
     // deployed version of amazon backend
     // baseURL: "https://amazon-api-deploy-5o05.onrender.com/"
     });
